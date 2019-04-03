df54c4835606be350c79a7c938e3221be36ad92a
Chris.N <cenunez47@gmail.com>
2019-04-03 16:24:28 -0700

    Added Layout component with initial styling.

---
+++ src/containers/Layout.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import styles from './Layout.module.css';
+
+class Layout extends Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <div className={styles.Layout}>
+                <h1>Placeholder</h1>
+            </div>
+        )
+    }
+}
+
+export default Layout;
