004b88cc6dff83857fc11b25ed1d2b549213ccde
Chris.N <cenunez47@gmail.com>
2019-04-04 09:28:43 -0700

    Created Articles stateless component.

---
+++ src/components/Articles/Articles.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './Articles.module.css';
+
+const articles = (props) => {
+
+    return (
+        <div className={classes.Articles}>
+            <h3>Title</h3>
+        </div>
+    )
+}
+
+export default articles;
