ad80935a25c28157bac99ff12314802f04f4ee5e
Chris.N <cenunez47@gmail.com>
2019-04-04 10:54:33 -0700

    Created Article component.

---
+++ src/components/Article/Article.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './Article.module.css';
+
+const article = (props) => {
+
+    return (
+        <div className={classes.Article}>
+            <h3>Placeholder</h3>
+        </div>
+    )
+}
+
+export default article;
