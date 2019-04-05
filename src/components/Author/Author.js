754365a3b9fd53dec6dc231a963bea9685023c90
Chris.N <cenunez47@gmail.com>
2019-04-05 14:46:51 -0700

    Created Author stateless component and stylesheet.

---
+++ src/components/Author/Author.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Author.module.css';
+
+const author = (props) => {
+
+ 
+    return (
+        <div className={classes.Author}>
+            <h1>Placeholder</h1>
+        </div>
+    )
+}
+
+export default author;
